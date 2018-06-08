0b209610299e2de69a731baeb838861aff0fa80c
wuid <3310975439@qq.com>
2018-06-08 20:36:29 +0800

    完善...

--- wav.go
+++ wav.go
@@ -1,5 +1,31 @@
-package wav
+package main
+
+import (
+	"os"
+	"fmt"
+)
 
 type Wav struct {
-	head *[]byte
+	dsize uint32
+	fsize uint32
+	head []byte
+	data []byte
+}
+
+func NewWav() *Wav {
+	wav := &Wav {}
+	wav.head = make([]byte, 44)
+	return wav
+}
+
+func (w *Wav) load(file string) {
+	f, _ := os.Open(file)
+	defer f.Close()
+	f.Read(w.head)
+	fmt.Println("head:\n", w.head)
+}
+
+func main() {
+	w := NewWav()
+	w.load("../lua/pb/8b/8b_1.wav")
 }
